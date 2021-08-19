44c6d8b01f056e1e5fd841ea96722eb7e7df4992
AdamKinyua <datascience.stud.2021@gmail.com>
2021-08-19 11:00:03 -0400

    Added variables_example.f90 file

---
+++ variables_example.f90
@@ -0,0 +1,15 @@
+program variables_example
+implicit none
+
+  !integer :: x
+  !integer :: y
+ ! integer :: z
+
+   ! x = 1
+   ! y = 1
+   ! z = 1
+
+    !   print*, "x = ", x, "y = ", y, "z = ", z
+   write(*,*) 'Hello World'
+
+end program variables_example
